986d7ca8f7c392f9af61d733b6b80b310f80f8c3
Arshal-Agarwal <ajagarwal2005@gmail.com>
2024-11-24 14:22:40 +0530

    Added About page

---
+++ frontend/app/Components/AboutDevs.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Image from 'next/image';
+
+const AboutDevs = () => {
+    return (
+        <div>
+
+            <div className="flex  space-x-48 pt-12 pl-48 pb-10 mb-12">
+                <div className='pt-12  bg-slate-100 pb-6 w-96 rounded-md shadow-xl flex flex-col items-center'>
+                    <Image
+                        alt="none"
+                        src="/Darshan.jpg"
+                        height={250}
+                        width={250}
+                        className="rounded-xl"
+                    />
+                    <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl text-center">Darshan Atkari</h3>
+                    <p className="mt-2 max-w-sm text-sm text-gray-700 text-center">
+                        Backend Expert with extensive knowledge of NodeJS and Databases.
+                    </p>
+                </div>
+
+                <div className='pt-12  bg-slate-100 pb-6 w-96 rounded-md shadow-xl  flex flex-col items-center'>
+                    <Image
+                        alt="none"
+                        src="/Arshal.jpg"
+                        height={250}
+                        width={250}
+                        className="rounded-xl"
+                    />
+                    <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl text-center">Arshal Agarwal</h3>
+                    <p className="mt-2 max-w-sm text-sm text-gray-700 text-center">
+                        Full-Stack Web Developer with boundless expertise in Front-end Development and Data-Structures.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default AboutDevs;
---
+++ frontend/app/pages/About/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import AboutCard from '@/app/Components/AboutCard'
+import AboutDevs from '@/app/Components/AboutDevs'
+
+ function About() {
+  return (
+    <div className='mt-4 bg-white dark:bg-gray-900 text-black dark:text-white'>
+      <AboutCard></AboutCard>
+      <h1 className="text-2xl text-center font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">About the Developers</h1>
+      <br />
+      <br />
+      <br />
+      <AboutDevs></AboutDevs>
+    </div>
+  )
+}
+
+export default About;
